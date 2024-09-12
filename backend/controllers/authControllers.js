d64ad7f472edacee8b523db542482b6171d8581a
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-09-12 06:55:16 +0100

    adding  signup route

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,43 @@
-export const signup = (req, res) => {
-  res.send("signup");
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const signup = async (req, res) => {
+  try {
+    const { name, username, email, password } = req.body;
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ messages: "Username is taken!" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //create a new user
+    const user = new User({ name, email, password: hashedPassword, username });
+    //save  the newly created user to the database
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res.cookie("jwtLinkedin", token, {
+      httpOnly: true, //prevent XSS attack
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", //prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production", //prevent Man In The Middle attacks
+    });
+    res.status(201).json({ message: "User registered successfully" });
+    //todo:send welcome email
+  } catch (error) {
+    console.log("Signup error: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
